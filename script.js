e03ecf0e1d26ed91cc84c871240448e67b3afa4f
Teresiah-cpu <terrywambo2018@gmail.com>
2022-03-04 20:01:11 +0300

    add a function in script.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+function calculator(){
+    let ageinput=document.getElementById("ageinput");
+    let date=ageinput.date.value
+    let gender=ageinput.gender.value
+    alert(date)
+    alert(gender)
+}
