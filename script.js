7dff172ce3ac256a98b066ecdcac4b2755d22cd4
Teresiah-cpu <terrywambo2018@gmail.com>
2022-03-05 18:07:27 +0300

    ft:add an images file

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+//function calculator(){
+   // let ageinput=document.getElementById("ageinput");
+    //let date=ageinput.date.value
+    //let gender=ageinput.gender.value
+    //alert(date)
+   // alert(gender)
+//}
 function calculator(){
-    let ageinput=document.getElementById("ageinput");
-    let date=ageinput.date.value
-    let gender=ageinput.gender.value
-    alert(date)
-    alert(gender)
+   let gender=form1.gender.selected.value
+   alert(gender)
+
 }
