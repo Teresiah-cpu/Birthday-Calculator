a98042fe2f12d85a9e68925dc9ef6398fdee0331
Teresiah-cpu <terrywambo2018@gmail.com>
2022-03-05 21:38:50 +0300

    add conditinal statements for female gender selection

--- script.js
+++ script.js
@@ -28,3 +28,32 @@ if (mm < 1 || mm> 12) {
     } else if (dd < 1 || dd> 31) {
         alert("enter a valid day");
         }
+
+ //formula calculation
+        var dayOfTheWeek = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;  
+        //names selection for females
+if (female) {
+if (dayOfTheWeek == 0) {
+  document.getElementById("results").innerHTML =
+  alert("You were born on Sunday hence your Akan name is.. " + femaleGender[0]);
+  } else if (dayOfTheWeek == 1) {
+  document.getElementById("results").innerHTML =
+  alert("You were born on Monday hence your Akan name is.. " + femaleGender[1]);
+  } else if (dayOfTheWeek == 2) {
+  document.getElementById("results").innerHTML =
+  alert("You were born on Tuesday hence your Akan name is.. " + femaleGender[2]);
+  } else if (dayOfTheWeek == 3) {
+  document.getElementById("results").innerHTML =
+  alert("You were born on Wednesday hence your Akan name is.. " + femaleGender[3]);
+  } else if (dayOfTheWeek == 4) {
+  document.getElementById("results").innerHTML =
+  alert("you were born on Thursday hence your Akan name is.. " + femaleGender[4]);
+  } else if (dayOfTheWeek == 5) {
+  document.getElementById("results").innerHTML =
+  alert("You were born on Friday hence your Akan name is.. " + femaleGender[5]);
+  } else if (dayOfTheWeek == 6) {
+  document.getElementById("results").innerHTML =
+  alert("You were born on Saturday hence your Akan name is.. " + femaleGender[6]);
+  }
+    
+}     
