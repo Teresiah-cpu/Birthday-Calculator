6f6c1d9130216114338aedc273bd3949e594302d
Teresiah-cpu <terrywambo2018@gmail.com>
2022-03-05 20:48:41 +0300

    add a function in script file

--- script.js
+++ script.js
@@ -5,8 +5,13 @@
     //alert(date)
    // alert(gender)
 //}
-function calculator(){
-   let gender=form1.gender.selected.value
-   alert(gender)
+function calc() {
+var maleGender = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleGender = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+var dd = document.getElementById("day").value;
+var year = document.getElementById("year").value;
+var mm = document.getElementById("month").value;
+var male = document.getElementById("male").checked;
+var female = document.getElementById("female").checked;
 }
